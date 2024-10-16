269700d0bde95f22422dfd189cd5094ad670738e
Hadrian Lazic <hadrian.lazic@gmail.com>
2024-10-16 10:11:06 -0700

    updates

---
+++ rustangles/src/main.rs
@@ -0,0 +1,39 @@
+extern crate glfw;
+
+use glfw::{Action, Context, Key};
+
+fn main() {
+   use glfw::fail_on_errors;
+let mut glfw = glfw::init(fail_on_errors!()).unwrap();
+
+    // create opengl window 
+    let (mut window, events) = glfw.create_window(300, 300, "Rustangles", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window.");
+
+        let vertices: [f32; 9] = [
+            -0.5, -0.5, 0.0, // bottum left
+             0.5, -0.5, 0.0, // bottum right
+             0.0,  0.5, 0.0, // top
+        ];
+
+    // make's windows context to current opengl thing
+    window.make_current();
+    window.set_key_polling(true);
+
+    while !window.should_close() {
+        // buffering frames
+        window.swap_buffers();
+
+        // event handling input 
+        glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            println!("{:?}", event);
+            match event {
+                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => { // input esc
+                    window.set_should_close(true)
+                },
+                _ => {},
+            }
+        }
+    }
+}
